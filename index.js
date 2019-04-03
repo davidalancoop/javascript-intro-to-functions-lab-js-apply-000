59965552ef6c662fcd00d372fc2887e22d889f2b
David Cooper <davidalancoop@gmail.com>
2019-04-03 20:22:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function logWhisper(string) {
   return console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string.toLowerCase) {
-  return "I can't hear you!"
-}
+function sayHiToGrandma(string) {
+  if (string.toLowerCase === string) {
+    return 'I can\'t hear you!'
+  }
